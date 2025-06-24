ee1ecd670ae2862923c1ab96506e2655c32a8a6c
Antonia Frey <antonia.frey@devoteam.com>
2025-06-24 09:04:28 +0200

    Cleanup imports

--- src/content/Hero/Hero.jsx
+++ src/content/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography, Avatar } from "@mui/material";
+import { Box, Typography, Avatar } from "@mui/material";
 import DownloadButton from "../DownloadButton/DownloadButton";
 import avatar from "../../assets/woman-technologist_1f469-200d-1f4bb.png";
 //import avatar from "../../assets/girl-with-laptop.jpg";
@@ -70,7 +70,7 @@ function Hero() {
         variant="h5"
         sx={{ width: { xs: "90%", md: "60%", lg: "40%" }, textAlign: "center" }}
       >
-        <b>Hey!</b> I'm a full-stack developer who loves building efficient,
+        <b>Hey!</b> I&apos;m a full-stack developer who loves building efficient,
         scalable, and user-friendly applications that make life easier.
         Mastering new technologies drives me, and I can become unstoppable when
         encountering a challenge.
@@ -79,7 +79,7 @@ function Hero() {
       <DownloadButton
         label="Download CV"
         color="primary.main"
-        url="https://raw.githubusercontent.com/thebughuntress/cv-overleaf/main/m cv.pdf"
+        url="https://raw.githubusercontent.com/thebughuntress/cv-overleaf/main/cv.pdf"
         downloadFileName="CV_AntoniaFrey.pdf"
         fallbackFile={cvFallback}
       />
--- src/content/Projects/Projects.jsx
+++ src/content/Projects/Projects.jsx
@@ -4,6 +4,12 @@ import { Typography, Box, Chip, Link } from "@mui/material";
 
 const projects = [
   {
+    name: "Flappy Bird",
+    description: "ML projects for practicing and learning different models and techniques",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/1200px-Scikit_learn_logo_small.svg.png",
+    repo: "https://github.com/thebughuntress/ml",
+  },
+  {
     name: "Machine Learning / AI",
     description: "ML projects for practicing and learning different models and techniques",
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/1200px-Scikit_learn_logo_small.svg.png",
